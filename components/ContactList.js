bd40c93ababea26f45f14fa8adfdcdc5674e33b1
George Ioakeimidis <giorgosioak95@gmail.com>
2018-07-14 01:06:34 +0300

    load contacts from a json via axios.get

--- components/ContactList.js
+++ components/ContactList.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-const ConstactList = () => (
-    <View>
-        <Text>ConstactList</Text>
-    </View>
-);
+class ConstactList extends Component {
+
+    state = { contacts: [] };
+
+    componentWillMount() {
+        axios.get('http://jsonplaceholder.typicode.com/users').then(response => this.setState({ contacts: response.data }));
+    }
+
+    renderContacts() {
+        this.state.contacts.map(contact =>
+            <Text key={contact.id}>{contact.name} {contact.phone}</Text>
+        );
+    }
+
+    render() {
+        return (
+            <View>
+                {this.renderContacts()}
+            </View>
+        );
+    }
+}
 
 export default ConstactList;
